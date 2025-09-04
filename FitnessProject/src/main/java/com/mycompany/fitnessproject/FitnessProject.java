e3cd7a8d88fd721b7d9fc6efaf407af8bf5975c4
ST10444941 <160851222+ST10444941@users.noreply.github.com>
2025-09-04 16:55:06 +0200

    Update FitnessProject.java

--- FitnessProject/src/main/java/com/mycompany/fitnessproject/FitnessProject.java
+++ FitnessProject/src/main/java/com/mycompany/fitnessproject/FitnessProject.java
@@ -28,9 +28,11 @@ public class FitnessProject {
         log.recordSession("M3", "Morning Flow");
 
         ReportGenerator report = new ReportGenerator(log);
-        report.printDailyReport();
+        report.printDailyReport(); //(Farrell, 2025)
     }
-}  
+}  //Reference List
+//Farrell, J.2023.Java Programming.Boston: Cengage.
+//Deepseek, 2024. Response to query about coding. [online] Available at: https://chat.deepseek.com/ [Accessed 2 September 2025]
       
     
 
